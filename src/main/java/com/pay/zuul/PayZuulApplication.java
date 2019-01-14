1f3bea32f869584f779876bec0d4a43f757e4260
chenwei <cw961590280@hotmail.com>
2019-01-14 14:57:59 +0800

    x

--- src/main/java/com/pay/zuul/PayZuulApplication.java
+++ src/main/java/com/pay/zuul/PayZuulApplication.java
@@ -2,8 +2,12 @@ package com.pay.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
+@EnableEurekaClient
 public class PayZuulApplication {
 
     public static void main(String[] args) {
