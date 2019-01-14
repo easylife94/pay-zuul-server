ddda74e230ca0355173813a3b6bcc68d88ab8b9f
chenwei <cw961590280@hotmail.com>
2019-01-14 14:42:37 +0800

    x

---
+++ src/main/java/com/pay/zuul/PayZuulApplication.java
@@ -0,0 +1,14 @@
+package com.pay.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayZuulApplication.class, args);
+    }
+
+}
+
